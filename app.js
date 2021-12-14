d50d3cd5ed439e05a39cd09a91b8d62eec4bc7e0
Dmitriy <dshishkunov.exceedteam@gmail.com>
2021-12-14 12:47:47 +0300

    added creating a new good

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 const apiRoutes = require("./src/modules/routes/routes");
 
 const url = 'mongodb+srv://ShishkunovD:restart987!@cluster0.8tbt5.mongodb.net/Module-3_final_project?retryWrites=true&w=majority';
--- src/modules/routes/routes.js
+++ src/modules/routes/routes.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const {
-  getAllGoods
+  getAllGoods,
+  createNewGood
 } = require('../controllers/task.controller');
 
 router.get('/getGoods', getAllGoods);
+router.post('/createGood', createNewGood);
 
 module.exports = router;
