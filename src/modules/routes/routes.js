125f7ca5b4f5eebd6dedb8b56bd442f69e85c658
Dmitriy <dshishkunov.exceedteam@gmail.com>
2021-12-14 13:40:48 +0300

    added delete our good

--- src/modules/routes/routes.js
+++ src/modules/routes/routes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const {
   getAllGoods,
-  createNewGood
+  createNewGood,
+  deleteGood
 } = require('../controllers/task.controller');
 
 router.get('/getGoods', getAllGoods);
 router.post('/createGood', createNewGood);
+router.delete('/deleteGood', deleteGood);
 
 module.exports = router;
