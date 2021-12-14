ee64326e7dd0cd5baeafcdc08ec41408d579870e
Dmitriy <dshishkunov.exceedteam@gmail.com>
2021-12-14 11:39:39 +0300

    fixed bugs after the first comments

--- src/modules/controllers/task.controller.js
+++ src/modules/controllers/task.controller.js
@@ -2,6 +2,6 @@ const Good = require('../../db/models/task/index');
 
 module.exports.getAllGoods = (req, res) => {
   Good.find().then(result => {
-      res.send({ data: result });
-  })
+    res.send({ data: result });
+  });
 }
--- src/modules/routes/routes.js
+++ src/modules/routes/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const {
-    getAllGoods
+  getAllGoods
 } = require('../controllers/task.controller');
 
 router.get('/getGoods', getAllGoods);
